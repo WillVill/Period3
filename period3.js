f3794fca601e51f4edec12876145523abd877e24
WillVill <bech.william2@gmail.com>
2016-03-17 14:21:46 +0100

    a

--- period3.js
+++ period3.js
@@ -1,1 +1,34 @@
-Explain generally what is meant by a NoSQL database
+/**
+ 
+ Explain generally what is meant by a NoSQL database
+
+A NoSQL databse is a mechanism to store and retrieve data (much like any database). What makes NoSQL databases unique compared
+to SQL databases or other databases in general is that it is created to support agility. It is built to support large amounts of
+new and changeing data. NoSQL databases have many different types of data in them unlike a classic SQL database, NoSQL adapts to 
+cahnge in terms of it expanding horizontally, meaning that if you were to add more servers to your database then NoSQL would 
+accomodate the new server and spread the data to best fit the new setup. Unlike SQL in which you would have to add onto the current
+server rather than being able to add new ones in; as the data does not accomodate new servers.
+
+Explain Pros & Cons in using a NoSQL database like MongoDB as your data store, compared to a traditional
+relational SQL Database like MySQL.
+
+
+Explain how databases like MongoDB and redis would be classified in the NoSQL world
+
+Explain reasons to add a layer like Mongoose, on top on of a schema-less database like MongoDB
+
+Explain, using relevant examples, the strategy for querying MongoDB (all CRUD operations)
+
+Demonstrate, using a REST-API, how to perform all CRUD operations on a MongoDB
+
+Explain the benefits from using Mongoose, and provide an example involving all CRUD operations
+
+Explain how redis "fits" into the NoSQL world, and provide an example of how to use it.
+
+Explain, using a relevant example, how redis (or a similar) can increase scalability (drastic) for a server using
+server side sessions
+
+Explain, using a relevant example, a full MEAN application including relevant test cases to test the REST-API
+(not on the production database)
+
+*/
