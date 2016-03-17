f2ff170399d0267c66942140bb1915bf48f30eb2
WillVill <bech.william2@gmail.com>
2016-03-17 15:43:11 +0100

    k

--- period3.js
+++ period3.js
@@ -7,13 +7,16 @@ to SQL databases or other databases in general is that it is created to support 
 new and changeing data. NoSQL databases have many different types of data in them unlike a classic SQL database, NoSQL adapts to 
 cahnge in terms of it expanding horizontally, meaning that if you were to add more servers to your database then NoSQL would 
 accomodate the new server and spread the data to best fit the new setup. Unlike SQL in which you would have to add onto the current
-server rather than being able to add new ones in; as the data does not accomodate new servers.
+server rather than being able to add new ones in; as the data does not accomodate new servers. NoSQL does not have to store data in
+tables. NoSql sacrificies some harddrive space for flexibility. 
 
 Explain Pros & Cons in using a NoSQL database like MongoDB as your data store, compared to a traditional
 relational SQL Database like MySQL.
 
 
 Explain how databases like MongoDB and redis would be classified in the NoSQL world
+
+MongoDB would be classified as a document NoSQL database. 
 
 Explain reasons to add a layer like Mongoose, on top on of a schema-less database like MongoDB
 
